77860e971e69194945852021f6d13b8e2c7b187e
MYCOMPUTER\nwuser <kiyukatarahayami912@gmail.com>
2018-09-07 12:41:25 +0900

    3.0版

---
+++ Assets/script/Camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = GameObject.Find("player").transform.position.x;
+        float y = transform.position.y;
+        transform.position = new Vector2(x, y);
+    }
+}
